55f1af600f003d549f542cfac3ba3178d387a1d1
Maria Clara Solek <117413554+mariasolek@users.noreply.github.com>
2024-09-20 02:28:17 -0300

    Update blackjack.vhd

--- 3-Semestre/SistemasDigitais/blackjack.vhd
+++ 3-Semestre/SistemasDigitais/blackjack.vhd
@@ -18,7 +18,7 @@ end entity blackjack;
 
 architecture comp of blackjack is
 
-    function seteSegmentos (carta: integer) return std_logic_vector is 
+function seteSegmentos (carta: integer) return std_logic_vector is 
 begin
     if(carta=0) then
         return "1000000";
@@ -54,8 +54,6 @@ begin
     end if;
 end seteSegmentos;  
 
-           
-      
     TYPE Estado is (START, ESPERA, C1, C2, C3, C4, PLAYER, DEALER, ACUMULAP, ACUMULAD, COMPARA, GANHA, PERDE, EMPATE);
     SIGNAL K : Estado;
     SIGNAL somaP, somaD : integer range 0 to 32 := 0; 
@@ -72,38 +70,38 @@ end seteSegmentos;
         if (KEY(1) = '0') then --aperta reset
             K <= START;
         elsif (KEY(0) 'EVENT AND KEY(0) = '0') then --aperta clk
-				case K is
+		case K is
                     when START =>
-						carta := 0;
-						cartavdd := 0;
-						ultCarta <= 0;
-						somaD <= 0;
-						somaP <= 0;
-						K <= ESPERA;
+			carta := 0;
+			cartavdd := 0;
+			ultCarta <= 0;
+			somaD <= 0;
+			somaP <= 0;
+			K <= ESPERA;
 							  
-					when ESPERA =>
-						cartar(0) := sw(0);
-						cartar(1) := sw(1);
-						cartar(2) := sw(2);
-					    cartar(3) := sw(3);
-						carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar));
-						if (carta >= 10) then 
+		when ESPERA =>
+			cartar(0) := sw(0);
+			cartar(1) := sw(1);
+			cartar(2) := sw(2);
+			cartar(3) := sw(3);
+			carta := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar));
+			if (carta >= 10) then 
                             carta := 10;
                         elsif (carta = 1) then 
                             carta := 11;
                             flagP := true;
                         end if;
-						ultCarta <= cartavdd;
-						K <= C1;
+			ultCarta <= cartavdd;
+			K <= C1;
 							  
-                    when C1 => --1 player
-						cartar(0) := sw(0);
-						cartar(1) := sw(1);
-						cartar(2) := sw(2);
-						cartar(3) := sw(3);
-						carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar));
+                when C1 => --1 player
+			cartar(0) := sw(0);
+			cartar(1) := sw(1);
+			cartar(2) := sw(2);
+			cartar(3) := sw(3);
+			carta := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar));
                         if (carta >= 10) then 
                             carta := 10;
                         elsif (carta = 1) then 
@@ -111,16 +109,16 @@ end seteSegmentos;
                             flagP := true;
                         end if;
                         somaP <= carta;
-						ultCarta <= cartavdd;
+			ultCarta <= cartavdd;
                         K <= C2;
 								
-                    when C2 => --1 dealer
-						cartar(0) := sw(0);
-						cartar(1) := sw(1);
-						cartar(2) := sw(2);
-						cartar(3) := sw(3);
-						carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar));
+                 when C2 => --1 dealer
+			cartar(0) := sw(0);
+			artar(1) := sw(1);
+			cartar(2) := sw(2);
+			cartar(3) := sw(3);
+			carta := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar));
                         if (carta >= 10) then 
                            carta := 10;
                         elsif (carta = 1) then 
@@ -128,16 +126,16 @@ end seteSegmentos;
                             flagD := true;
                         end if;
                         somaD <= carta;
-						ultCarta <= cartavdd;
+			ultCarta <= cartavdd;
                         K <= C3;
 								
-                    when C3 => -- 2 player
-						cartar(0) := sw(0);
-						cartar(1) := sw(1);
-						cartar(2) := sw(2);
-						cartar(3) := sw(3);
-						carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar));
+                 when C3 => -- 2 player
+			cartar(0) := sw(0);
+			cartar(1) := sw(1);
+			cartar(2) := sw(2);
+			cartar(3) := sw(3);
+			carta := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar));
                         if (carta >= 10) then 
                             carta := 10;
                         elsif (carta = 1) then 
@@ -148,16 +146,16 @@ end seteSegmentos;
                             carta := carta - 10;
                         end if; 
                         somaP <= somaP + carta;
-						ultCarta <= cartavdd;
+			ultCarta <= cartavdd;
                         K <= C4;
 								
-                    when C4 => --2 dealer
-						cartar(0) := sw(0);
-						cartar(1) := sw(1);
-						cartar(2) := sw(2);
-						cartar(3) := sw(3);
-						carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar));
+                  when C4 => --2 dealer
+			cartar(0) := sw(0);
+			cartar(1) := sw(1);
+			cartar(2) := sw(2);
+			cartar(3) := sw(3);
+			carta := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar));
                         if (carta >= 10) then 
                             carta := 10;
                         elsif (carta = 1) then --verifica ais
@@ -168,19 +166,19 @@ end seteSegmentos;
                             carta := carta - 10;
                         end if;
                         somaD <= somaD + carta;
-						ultCarta <= cartavdd;
+			ultCarta <= cartavdd;
                         K <= PLAYER;
 								
                     when PLAYER => 
-						if (somaP > 21) then 
+			if (somaP > 21) then 
                             K <= PERDE;
-						end if;
+			end if;
                         if (sw(8) = '1') then --HIT
                             K <= ACUMULAP;
                         elsif (sw(9) = '1') then --STAY
                             K <= DEALER;
                         end if;
-						ultCarta <= cartavdd;
+			ultCarta <= cartavdd;
 
                     when ACUMULAP =>
                         cartar(0) := sw(0);
@@ -188,7 +186,7 @@ end seteSegmentos;
                         cartar(2) := sw(2);
                         cartar(3) := sw(3);
                         carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar));
                         if (carta >= 10) then 
                             carta := 10;
                         end if;
@@ -203,14 +201,14 @@ end seteSegmentos;
                         else
                             K <= PLAYER;
                         end if;
-						ultcarta <= cartavdd;
+			ultcarta <= cartavdd;
 								
                     when DEALER =>  
-						if (somaD >= 17) then 
+			if (somaD >= 17) then 
                             K <= COMPARA;
-						else
-							K <= ACUMULAD;
-						end if;
+			else
+			    K <= ACUMULAD;
+			end if;
 								
                     when ACUMULAD =>
                         if (somaD >= 17) then 
@@ -218,12 +216,12 @@ end seteSegmentos;
                         else 
                             K <= DEALER;
                         end if;
-						cartar(0) := sw(0);
-						cartar(1) := sw(1);
-						cartar(2) := sw(2);
-						cartar(3) := sw(3);
-						carta := to_integer(unsigned(cartar));
-						cartavdd := to_integer(unsigned(cartar)); --signal, que recebe a variável cartar, usado mais tarde para mostrar no display o número dda carta
+			cartar(0) := sw(0);
+			cartar(1) := sw(1);
+			cartar(2) := sw(2);
+			cartar(3) := sw(3);
+			carta := to_integer(unsigned(cartar));
+			cartavdd := to_integer(unsigned(cartar)); --signal, que recebe a variável cartar, usado mais tarde para mostrar no display o número da carta
                         if (carta >= 10) then 
                             carta := 10;
                         end if;
@@ -233,11 +231,11 @@ end seteSegmentos;
                             carta := 11;
                         end if;
                         somaD <= somaD + carta;
-						ultcarta <= cartavdd;
+			ultcarta <= cartavdd;
 
                     when COMPARA =>
-						if (somaD > 21) then 
-							K <= GANHA;
+			if (somaD > 21) then 
+			    K <= GANHA;
                         elsif (somaP = somaD) then 
                             K <= EMPATE;
                         elsif (somaD < somaP) then 
@@ -263,93 +261,93 @@ begin
             HEX1 <= "1111111";
             HEX2 <= "1111111";
             HEX3 <= "0010010";
-			ledG <= "00000000";
-			ledR <= "0000000001";
-		  when ESPERA =>
-			HEX0 <= "1111111";
+	    ledG <= "00000000";
+	    ledR <= "0000000001";
+	 when ESPERA =>
+	    HEX0 <= "1111111";
             HEX1 <= "1111111";
             HEX2 <= "1111111";
             HEX3 <= "0000110";
-			ledG <= "00000000";
-			ledR <= "1000000001";
+	    ledG <= "00000000";
+	    ledR <= "1000000001";
         when C1 =>
             HEX0 <= seteSegmentos(ultcarta);
             HEX1 <= "1111111";
             HEX2 <= "1111001";
             HEX3 <= "1000110";
-			ledG <= "00000000";
-			ledR <= "0000000010";
+            ledG <= "00000000";
+	    ledR <= "0000000010";
         when C2 =>
             HEX0 <= seteSegmentos(ultcarta);
             HEX1 <= "1111111";
             HEX2 <= "0100100";
             HEX3 <= "1000110";
-			ledG <= "00000000";
-			ledR <= "0000000100";
+	    ledG <= "00000000";
+	    ledR <= "0000000100";
         when C3 =>
             HEX0 <= seteSegmentos(ultcarta);
             HEX1 <= "1111111";
             HEX2 <= "0110000";
             HEX3 <= "1000110";
             ledG <= "00000000";
-			ledR <= "0000001000";
+	    ledR <= "0000001000";
         when C4 =>
             HEX0 <= seteSegmentos(ultcarta);
             HEX1 <= "1111111";
             HEX2 <= "0011001";
             HEX3 <= "1000110";
-			ledG <= "00000000";
-			ledR <= "0000010000";
+	    ledG <= "00000000";
+	    ledR <= "0000010000";
         when PLAYER =>
             HEX0 <= seteSegmentos(somaP mod 10);
             HEX1 <= seteSegmentos(somaP / 10);
             HEX2 <= "1111111";
             HEX3 <= "0001100";
-			ledG <= "00000000";
-			ledR <= "0000100000";
+	    ledG <= "00000000";
+	    ledR <= "0000100000";
         when DEALER =>
             HEX0 <= seteSegmentos(somaD mod 10);
             HEX1 <= seteSegmentos(somaD / 10);
             HEX2 <= "1111111";
             HEX3 <= "0100001";
-			ledG <= "00000000";
-			ledR <= "0001000000";
+	    ledG <= "00000000";
+	    ledR <= "0001000000";
         when ACUMULAP => --dizer somas
             HEX0 <= seteSegmentos(ultcarta);
             HEX1 <= seteSegmentos(somaP mod 10);
             HEX2 <= seteSegmentos(somaP / 10);
             HEX3 <= "0001100";
-			ledG <= "00000000";
-			ledR <= "0010000000";
+	    ledG <= "00000000";
+	    ledR <= "0010000000";
         when ACUMULAD =>
             HEX0 <= seteSegmentos(ultcarta);
             HEX1 <= seteSegmentos(somaD mod 10);
             HEX2 <= seteSegmentos(somaD / 10);
             HEX3 <= "0100001";
-			ledG <= "00000000";
-			ledR <= "0100000000";
+            ledG <= "00000000";
+	    ledR <= "0100000000";
         when COMPARA =>
             HEX0 <= seteSegmentos(somaD mod 10);
             HEX1 <= seteSegmentos(somaD / 10);
             HEX2 <= seteSegmentos(somaP mod 10);
             HEX3 <= seteSegmentos(somaP / 10);
-			ledG <= "00000000";
-			ledR <= "1000000000";
+	    ledG <= "00000000";
+	    ledR <= "1000000000";
         when GANHA => 
-			HEX0 <= "0001000";
+	    HEX0 <= "0001000";
             HEX1 <= "0001000";
             HEX2 <= "0000011";
             HEX3 <= "0000110";
             ledG <= "11111111";
-			ledR <= "0000000000";
+	    ledR <= "0000000000";
         when PERDE =>
-			HEX0 <= "0000110";
+	    HEX0 <= "0000110";
             HEX1 <= "0010010";
             HEX2 <= "1000000";
             HEX3 <= "1000111";
             ledR <= "1111111111";
         when EMPATE =>
-		  	HEX0 <= "0111111";
+	    HEX0 <= "0111111";
             HEX1 <= "0111111";
             HEX2 <= "0111111";
             HEX3 <= "0111111";
